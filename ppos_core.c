5d1dc047167df53944aea7d8df2c6c4ebae9ae23
Allan Cedric <allan.py3000@gmail.com>
2021-10-16 12:22:57 -0300

    feat: p4 is done

--- ppos_core.c
+++ ppos_core.c
@@ -1,16 +1,22 @@
+// --- Implementação dos principais mecanismos do PingPongOS ---
+
+// Allan Cedric G. B. Alves da Silva - GRR20190351
+
 #include "ppos.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_PRIOR -20
-#define MIN_PRIOR (-MAX_PRIOR)
-#define DEFAULT_PRIOR 0
+// --- Macros ---
+#define MAX_PRIO -20
+#define MIN_PRIO (-MAX_PRIO)
+#define DEFAULT_PRIO 0
 #define ALPHA -1
+
 #define MAIN_PID 0
-#define STACK_SIZE 64 * 1024
+#define STACK_SIZE (64 * 1024)
 
+// --- Definições de tipos convenientes ---
 typedef char byte_t;
-
 typedef enum task_states_t
 {
     TERM,
@@ -18,10 +24,16 @@ typedef enum task_states_t
     SUSPENSE
 } task_states_t;
 
+// --- Variáveis globais do sistema ---
 int pid = 0, num_user_tasks = 0;
 task_t main_task, dispatcher, *current_task;
 task_t *user_tasks_queue;
 
+/*!
+    @brief  Libera memória de uma tarefa.
+
+    @param  task    Endereço do ponteiro que aponta para uma tarefa.
+*/
 void free_task(task_t **task)
 {
     free((*task)->context.uc_stack.ss_sp);
@@ -29,56 +41,76 @@ void free_task(task_t **task)
     (*task)->context.uc_stack.ss_size = 0;
 }
 
+/*!
+    @brief  Imprime na saída padrão o ID e a prioridade dinâmica de uma tarefa.
+
+    @param  elem    Ponteiro para uma estrutura genérica.
+*/
 void print_task_id(void *elem)
 {
-    task_t *task = (task_t *)elem;
-    printf("%i:%i", task->id, task->dynam_prior);
+    task_t *task = elem;
+    fprintf(stdout, "%i:%i", task->id, task->dynam_prio);
 }
 
+/*!
+    @brief  Rotina do escalanador de tarefas (Escalonamento por prioridades dinâmicas).
+
+    @return Endereço da tarefa escolhida. 
+            Caso não tenha tarefas a executar retorna NULL.
+*/
 task_t *scheduler()
 {
     // Caso a fila de execução esteja vazia
     if (!user_tasks_queue)
         return NULL;
 
-    int max_prior = user_tasks_queue->dynam_prior;
     task_t *cur = user_tasks_queue, *choosen_task = cur;
+    int max_prio = user_tasks_queue->dynam_prio;
 
     // Varre toda a fila de execução
     while ((cur = cur->next) != user_tasks_queue)
     {
         // Prioridade em escala negativa
-        if (cur->dynam_prior < max_prior)
+        if (cur->dynam_prio < max_prio)
         {
             // Envelhece a tarefa escolhida anteriormente
-            if (choosen_task->dynam_prior > MAX_PRIOR)
-                choosen_task->dynam_prior += ALPHA;
+            if (choosen_task->dynam_prio > MAX_PRIO)
+                choosen_task->dynam_prio += ALPHA;
 
             // Atualiza para uma nova tarefa
-            max_prior = cur->dynam_prior;
             choosen_task = cur;
+            max_prio = cur->dynam_prio;
         }
-        else if (cur->dynam_prior > MAX_PRIOR)
-            cur->dynam_prior += ALPHA;
+        else if (cur->dynam_prio > MAX_PRIO)
+            cur->dynam_prio += ALPHA;
     }
-    choosen_task->dynam_prior = choosen_task->static_prior;
+    choosen_task->dynam_prio = choosen_task->static_prio;
 
 #ifdef DEBUG
     fprintf(stdout, "PPOS (scheduler): task %i choosen!\n", choosen_task->id);
-    queue_print("PPOS (scheduler): Ready user's tasks ->", (queue_t *)user_tasks_queue, print_task_id);
+    queue_print("PPOS (scheduler): User's ready tasks -> ", (queue_t *)user_tasks_queue, print_task_id);
 #endif
 
     return choosen_task;
-    // return user_tasks_queue; // Retorna a cabeça (Escalonador FCFS)
 }
 
+/*!
+    @brief  Rotina do despachante de tarefas.
+*/
 void dispatcher_proc(void *arg)
 {
-    while (num_user_tasks)
-    {
+
 #ifdef DEBUG
-        queue_print("PPOS (dispatcher): Ready user's tasks ->", (queue_t *)user_tasks_queue, print_task_id);
+    fprintf(stdout, "PPOS (dispatcher): Dispatcher was launched!\n");
 #endif
+
+    while (user_tasks_queue)
+    {
+
+#ifdef DEBUG
+        queue_print("PPOS (dispatcher): User's ready tasks -> ", (queue_t *)user_tasks_queue, print_task_id);
+#endif
+
         task_t *next_task = scheduler();
 
         if (next_task)
@@ -103,6 +135,7 @@ void dispatcher_proc(void *arg)
 
 void ppos_init()
 {
+
 #ifdef DEBUG
     fprintf(stdout, "PPOS (ppos_init): OS initialization...\n");
 #endif
@@ -115,8 +148,8 @@ void ppos_init()
     main_task.next = NULL;
     main_task.id = MAIN_PID;
     main_task.state = READY;
-    main_task.static_prior = MAX_PRIOR;
-    main_task.dynam_prior = MAX_PRIOR;
+    main_task.static_prio = MAX_PRIO;
+    main_task.dynam_prio = MAX_PRIO;
 
     // Inicialização da tarefa atual
     current_task = &main_task;
@@ -141,13 +174,19 @@ void ppos_init()
 
 int task_create(task_t *task, void (*start_func)(void *), void *arg)
 {
+    if (!task)
+    {
+        fprintf(stderr, "Error (task_create): There is no task structure!\n");
+        return -1;
+    }
+
     task->prev = NULL;
     task->next = NULL;
     task->id = ++pid;
     task->state = READY;
-    task->static_prior = DEFAULT_PRIOR;
-    task->dynam_prior = DEFAULT_PRIOR;
-    getcontext(&task->context); // Salva um contexto genérico
+    task->static_prio = DEFAULT_PRIO;
+    task->dynam_prio = DEFAULT_PRIO;
+    getcontext(&task->context); // Salva o contexto atual
 
     // Inicialização da stack
     byte_t *stack = (byte_t *)malloc(STACK_SIZE);
@@ -178,11 +217,12 @@ int task_create(task_t *task, void (*start_func)(void *), void *arg)
     fprintf(stdout, "PPOS (task_create): task %i (%p) was created successfully!\n", task->id, task);
 #endif
 
-    return 0;
+    return task->id;
 }
 
 void task_exit(int exitCode)
 {
+
 #ifdef DEBUG
     fprintf(stdout, "PPOS (task_exit): task %i is terminating...\n", task_id());
 #endif
@@ -226,23 +266,29 @@ int task_id()
 
 void task_yield()
 {
+
 #ifdef DEBUG
     fprintf(stdout, "PPOS (task_yield): task %i yields the CPU!\n", task_id());
 #endif
-    if (user_tasks_queue && task_id() > 1)
-        user_tasks_queue = user_tasks_queue->next;
+
     task_switch(&dispatcher);
 }
 
-void task_setprio (task_t *task, int prio)
+void task_setprio(task_t *task, int prio)
 {
-    if(task)
-        task->static_prior = prio;
+    if (task)
+    {
+        task->static_prio = prio;
+        task->dynam_prio = prio;
+    }
     else
-        current_task->static_prior = prio;
+    {
+        current_task->static_prio = prio;
+        current_task->dynam_prio = prio;
+    }
 }
 
-int task_getprio (task_t *task)
+int task_getprio(task_t *task)
 {
-    return (task ? task->static_prior : current_task->static_prior);
+    return (task ? task->static_prio : current_task->static_prio);
 }
